cd267fe9739dbfa3590a73ae06d185713d4aa212
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-10-31 17:03:29 -0400

    added technincaly palindrome cases(single letter string)

--- reverseString.test.js
+++ reverseString.test.js
@@ -6,4 +6,6 @@ test('exists', ()=> {
 
 test('palindrome', ()=> {
     expect(reverseString('racecar')).toBe('racecar')
+    expect(reverseString('dad')).toBe('dad')
+    expect(reverseString('J')).toBe('J')
 })
