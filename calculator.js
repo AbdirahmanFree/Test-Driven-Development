2ccb93ade5911af0e154748a21357135622bc713
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-11-01 12:49:29 -0400

    completed and tested divide and multiply functions

--- calculator.js
+++ calculator.js
@@ -8,7 +8,14 @@ class Calculator {
     subtract(x,y){
         return x-y
     }
+    divide(x,y){
+        return x/y
+    }
+    multiply(x,y){
+        return x*y
+    }
 }
+
 
 
 
--- calculator.test.js
+++ calculator.test.js
@@ -13,3 +13,19 @@ test('subtract function', ()=>{
     expect(calculator.subtract(-3,5)).toBe(-8)
     expect(calculator.subtract(-3,-5)).toBe(2)
 })
+
+test('divide function', ()=> {
+    expect(calculator.divide(1,1)).toBe(1)
+    expect(calculator.divide(10,2)).toBe(5)
+    expect(calculator.divide(1,2)).toBe(0.5)
+    expect(calculator.divide(1/0)).toBe(NaN)
+   
+})
+
+test('multiply function', ()=> {
+    expect(calculator.multiply(0,0)).toBe(0)
+    expect(calculator.multiply(1,1)).toBe(1)
+    expect(calculator.multiply(4,5)).toBe(20)
+    expect(calculator.multiply(-3,-4)).toBe(12)
+    expect(calculator.multiply(-1,12345)).toBe(-12345)
+})
