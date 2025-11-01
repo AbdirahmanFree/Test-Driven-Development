eab06c4a32140d59626c1061a1edd2daaae94e50
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-11-01 14:46:21 -0400

    finished ceasercipher function and testing

--- ceaserCipher.js
+++ ceaserCipher.js
@@ -1,6 +1,63 @@
 function ceaserCipher(str,offset){
+    if(offset != 0){
+        let lower = []
+        let upper = []
+        for (let i = 0; i<26;i++){
+            lower.push(String.fromCharCode("a".charCodeAt(0)+i))
+            upper.push(String.fromCharCode("A".charCodeAt(0)+i))
+        }
+
+        let index;
+        let string = ""
+        for(let i = 0; i < str.length; i++){
+            if(isLower(str.charAt(i))){
+                index = getIndex(str.charAt(i), offset)
+                string = string + lower[index]
+             }
+
+            else if(isUpper(str.charAt(i))){
+                index = getIndex(str.charAt(i), offset)
+                string = string + upper[index]
+            }
+            else{
+                string = string + str.charAt(i)
+            }
+
+        }
+        return string 
+        
+
+    }
+    return str
     
-    
+}
+
+function isLower(char){
+    if(char.charCodeAt(0) <="z".charCodeAt(0) && char.charCodeAt(0) >= "a".charCodeAt(0)){
+        return true
+    }
+    return false
+}
+
+function isUpper(char){
+    if(char.charCodeAt(0) <="Z".charCodeAt(0) && char.charCodeAt(0) >= "A".charCodeAt(0)){
+        return true
+    }
+    return false
+}
+
+function getIndex(char, offset){
+    let charCode
+    if(isUpper(char)){
+        charCode = char.charCodeAt(0) - "A".charCodeAt(0)
+    }
+    else if(isLower(char)){
+        charCode = char.charCodeAt(0) - "a".charCodeAt(0)
+    }
+    let index = offset + charCode
+    index > 25 ? index = (index %25) -1 : index 
+    return index
+
 }
 
 module.exports = ceaserCipher
--- ceaserCipher.test.js
+++ ceaserCipher.test.js
@@ -2,4 +2,39 @@ const ceaserCipher = require("./ceaserCipher")
 
 test('one letter no offset', ()=> {
     expect(ceaserCipher("a",0)).toBe("a")
+    expect(ceaserCipher("b",0)).toBe("b")
+    expect(ceaserCipher("c",0)).toBe("c")
 })
+
+test('full words no offset',()=> {
+    expect(ceaserCipher("hello",0)).toBe("hello")
+})
+
+test('one letter with offset', () => {
+    expect(ceaserCipher("a",1)).toBe("b")
+    expect(ceaserCipher("b",3)).toBe("e")
+    expect(ceaserCipher("z",-1)).toBe("y")
+    expect(ceaserCipher("A",4)).toBe("E")
+    expect(ceaserCipher("z",1)).toBe("a")
+    expect(ceaserCipher("w",2)).toBe("y")
+
+    expect(ceaserCipher("W",3)).toBe("Z")
+})
+
+test('punctuation',()=> {
+    expect(ceaserCipher(",",0)).toBe(",")
+    expect(ceaserCipher(",",4)).toBe(",")
+    expect(ceaserCipher(";",23)).toBe(";")
+})
+
+test('words',()=> {
+    expect(ceaserCipher("ab",1)).toBe("bc")
+    expect(ceaserCipher("xyz",3)).toBe("abc")
+    expect(ceaserCipher('HeLLo', 3)).toBe("KhOOr")
+    
+})
+
+test('sentences', () => {
+    expect(ceaserCipher('Hello, World!', 3)).toBe('Khoor, Zruog!')
+})
+
