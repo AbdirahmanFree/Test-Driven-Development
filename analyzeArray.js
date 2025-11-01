1e9a2f05a90a53adfdb3086ef27a3917d3277dc9
AbdirahmanFree <abdirahmanm.abdulahi@gmail.com>
2025-11-01 15:49:55 -0400

    finished and tested analyzeArray function

--- analyzeArray.js
+++ analyzeArray.js
@@ -1,5 +1,53 @@
 function analyzeArray(array){
-    return
+    let object = {};
+    if (array.length == 1){
+        object = {
+            average: array[0],
+            min: array[0],
+            max: array[0],
+            length: 1
+        }
+    }
+    else if(array.length == 0){
+        object = {
+            average: null,
+            min: null,
+            max: null,
+            length: 0
+        }
+    }
+    else {
+        object = {
+            average: averageArr(array),
+            min: minArr(array),
+            max: maxArr(array),
+            length: array.length
+        }
+    }
+
+    return object
+    
+    
+    
+}
+function averageArr(array){
+    sum = array.reduce((acc, val) =>{
+        return acc + val
+    }, 0)
+    return sum/array.length
+}
+
+function minArr(array){
+    return array.reduce((min,val) => {
+        return val < min ? val: min
+    }, array[0])
+}
+
+
+function maxArr(array){
+   return array.reduce((max, val)=>{
+        return val > max ? val : max
+    },array[0])
 }
 
 module.exports = analyzeArray
--- analyzeArray.test.js
+++ analyzeArray.test.js
@@ -1,5 +1,41 @@
 const analyzeArray = require("./analyzeArray")
 
-test('exists', ()=>{
-    expect(analyzeArray([1])).toBe(undefined)
+test('empty array', ()=> {
+    expect(analyzeArray([])).toEqual({
+        average: null,
+        min: null,
+        max: null,
+        length: 0
+    })
+})
+
+test('array with one element', ()=> {
+    expect(analyzeArray([1])).toEqual({
+        average: 1,
+        min: 1,
+        max: 1,
+        length: 1
+    })
+
+    expect(analyzeArray([2])).toEqual({
+        average: 2,
+        min: 2,
+        max: 2,
+        length: 1
+    })
+
+    expect(analyzeArray([-8])).toEqual({
+        average: -8,
+        min: -8,
+        max: -8,
+        length: 1
+    })
+
+    
+    
+})
+
+test('multiple elements', () => {
+    expect(analyzeArray([1,2,3,4,5])).toEqual({average: 3, length: 5, max: 5, min: 1})
+    expect(analyzeArray([1,8,3,4,2,6])).toEqual({average: 4, length:6 , max: 8, min: 1})
 })
